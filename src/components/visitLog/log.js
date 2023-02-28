e48d8f2f859655ac975408e164cb19883c2258f3
OhSeChan <seb_s19@naver.com>
2023-02-28 15:39:47 +0900

    feat : 통신 테스트 코드 추가 및 메뉴바 하단 고정
    - 방명록 통신 테스트 코드 추가 이후 백엔드 통신 후 수정 예정
    
    - 메뉴바 하단 고정 css 수정

--- src/components/visitLog/log.js
+++ src/components/visitLog/log.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import '../../styles/visitLog/log.css'
 
-const log = () => {
-    return (
-			<div className='log_container'>
-				<div className='log_time'>
-					2023.03.03 3:18PM
-				</div>
-				<div className='log_commentLine'>
-					<div className='log_timeCircle'>ㅤ</div>
-					<hr className='log_line'/>
-					<div className='log_comment'>
-						학과 친구들을 많이 알게된 계기가 되어 너무 좋았어요~~ 학과생활이 너무 기대돼요.
-					</div>
-					<hr className='log_line' id='end'/>
-				</div>
+const log = ({logNum,logTime,comment}) => {
+
+	let commentColor;
+
+	switch (logNum % 3) {
+		case 0:
+			commentColor = '#F1F0FF'
+			break;
+		case 1:
+			commentColor = "#F3FFF2"
+			break;
+		case 2:
+			commentColor = "#EDF6FF"
+			break;
+		default:
+			commentColor = '#F1F0FF'
+			break;
+	}
+
+  return (
+		<div className='log_container'>
+			<div className='log_time'>
+				{logTime}
 			</div>
-    );
+			<div className='log_commentLine'>
+				<div className='log_timeCircle'>ㅤ</div>
+				<hr className='log_line'/>
+				<div className='log_comment' style={{backgroundColor : commentColor}}>
+					{comment}
+				</div>
+				<hr className='log_line' id='end'/>
+			</div>
+		</div>
+  );
 };
 
 export default log;
