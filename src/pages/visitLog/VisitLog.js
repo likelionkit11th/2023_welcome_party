96f8d55d997f21f0ef8d1ab20e08c972e581b5e3
Moyujin <scab12341@gmail.com>
2023-03-07 22:47:22 +0900

    fixed: 오타 수정

--- src/pages/visitLog/VisitLog.js
+++ src/pages/visitLog/VisitLog.js
@@ -37,7 +37,7 @@ const VisitLog = () => {
       <div className="visitLog_page_header">
         <h1>방명록</h1>
         <div>
-          오늘의 소감과 함계 방명록을 작성해 주세요.
+          오늘의 소감과 함께 방명록을 작성해 주세요.
           <br />
           심한 욕설이 포함된 비난글은 삭제 조치 될 수 있습니다.
         </div>
